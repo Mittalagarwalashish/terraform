0b50eea8b4e3fba69bd58437aaa8e095c2f7659b
Mittalagarwalashish <104975349+Mittalagarwalashish@users.noreply.github.com>
2022-10-10 00:29:33 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -164,3 +164,28 @@ resource "aws_security_group" "private-allow" {
     Name = "private"
   }
 }
+
+
+resource "aws_instance" "vm-public" {
+  ami           = "ami-09208e69ff3feb1db" # us-west-2
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.Public.id
+  key_name = "terraformwest"
+  vpc_security_group_ids = [aws_security_group.public-allow.id]
+  depends_on = [aws_vpc.vpc1.id]
+  tags = {
+    Name = "public-vm"
+  } 
+}
+
+resource "aws_instance" "vm-private" {
+  ami           = "ami-09208e69ff3feb1db" # us-west-2
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.Private.id
+  key_name = "terraformwest"
+  vpc_security_group_ids = [aws_security_group.private-allow.id]
+  depends_on = [aws_vpc.vpc1.id]
+  tags = {
+    Name = "private-vm"
+  } 
+}
